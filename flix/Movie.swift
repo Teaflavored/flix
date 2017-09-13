d90211b51b03b28a47f0b761b8c222d09710a088
Auster Chen <auster.s.chen@gmail.com>
2017-09-13 00:20:03 -0700

    render basic movie info

---
+++ flix/Movie.swift
@@ -0,0 +1,58 @@
+//
+//  Movie.swift
+//  flix
+//
+//  Created by Auster Chen on 9/12/17.
+//  Copyright © 2017 Auster Chen. All rights reserved.
+//
+
+import Foundation
+
+class Movie {
+    var id: Int?
+    var overview: String?
+    var title: String?
+    var posterPath: String?
+
+    private let BASE_IMAGE_URL =  "https://image.tmdb.org/t/p/w500"
+
+    init(config: NSDictionary) {
+        id = config["id"] as? Int
+        overview = config["overview"] as? String
+        title = config["title"] as? String
+        posterPath = config["poster_path"] as? String
+    }
+
+    func getImageUrl() -> String? {
+        if let _posterPath = posterPath {
+            return "\(BASE_IMAGE_URL)\(_posterPath)"
+        }
+        
+        return nil
+    }
+
+    class func fetchMovies(successCallBack: @escaping ([Movie]) -> (), errorCallBack: ((Error?) -> ())?) {
+        let apiKey = "cd4e181e550bd1b9eb536ae2fd16907b"
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task: URLSessionDataTask = session.dataTask(with: request) { (data: Data?, response: URLResponse?, error: Error?) in
+            if let error = error {
+                errorCallBack?(error)
+            } else if let data = data,
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? NSDictionary {
+                if let results = dataDictionary["results"] as? [NSDictionary] {
+                    var movies: [Movie] = []
+                    for result in results {
+                        movies.append(Movie(config: result))
+                    }
+
+                    successCallBack(movies)
+                } else {
+                    successCallBack([])
+                }
+            }
+        }
+        task.resume()
+    }
+}
--- flix/ViewController.swift
+++ flix/ViewController.swift
@@ -8,16 +8,48 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,
+UITableViewDataSource,
+UITableViewDelegate {
+
+    var movies: [Movie] = []
+    @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        Movie.fetchMovies(successCallBack: {
+            (movies: [Movie]) -> Void in
+            self.movies = movies
+            self.tableView.reloadData()
+        }, errorCallBack: nil)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let movieCell = tableView.dequeueReusableCell(withIdentifier: "MovieCell") as! MovieTableViewCell
+
+        if let title =  movies[indexPath.row].title {
+            movieCell.titleLabel.text = title
+        }
+
+        if let overview = movies[indexPath.row].overview {
+            movieCell.overviewLabel.text = overview
+        }
+
+        if let posterImageUrl = movies[indexPath.row].getImageUrl() {
+            print(posterImageUrl)
+        }
+
+        return movieCell
     }
 }
 
