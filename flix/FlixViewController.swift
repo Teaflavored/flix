ef88f9e566868a70ec2a87b6d1caaafa89ce4a67
Auster Chen <auster.s.chen@gmail.com>
2017-09-16 01:58:16 -0700

    add search bar

--- flix/FlixViewController.swift
+++ flix/FlixViewController.swift
@@ -12,12 +12,16 @@ import VHUD
 
 class FlixViewController: UIViewController,
     UITableViewDataSource,
-UITableViewDelegate {
+UITableViewDelegate,
+UISearchResultsUpdating {
     
     let refreshControl = UIRefreshControl()
+    let searchController = UISearchController(searchResultsController: nil)
     var content = VHUDContent(.loop(3.0))
 
     var movies: [Movie] = []
+    var filteredMovies: [Movie] = []
+    var searchText: String? = ""
     var endpoint: String = "now_playing"
     @IBOutlet weak var tableView: UITableView!
     
@@ -31,6 +35,10 @@ UITableViewDelegate {
         content.background = .color(#colorLiteral(red: 0.937254902, green: 0.937254902, blue: 0.9568627451, alpha: 0.7))
         content.style = .light
 
+        searchController.searchResultsUpdater = self
+        searchController.dimsBackgroundDuringPresentation = false
+        tableView.tableHeaderView = searchController.searchBar
+
         // Setting up pull to refresh
         refreshControl.addTarget(self, action: #selector(refreshControlAction(_:)), for: UIControlEvents.valueChanged)
         tableView.insertSubview(refreshControl, at: 0)
@@ -38,7 +46,6 @@ UITableViewDelegate {
         // Initial movie fetch
         fetchMovies()
     }
-
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -61,24 +68,55 @@ UITableViewDelegate {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return movies.count
+        return getMoviesToDisplay().count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let moviesListToUse = getMoviesToDisplay()
         let movieCell = tableView.dequeueReusableCell(withIdentifier: "MovieCell") as! MovieTableViewCell
-        movieCell.updateCellContentWithMovie(movie: movies[indexPath.row])
+        movieCell.updateCellContentWithMovie(movie: moviesListToUse[indexPath.row])
         return movieCell
     }
 
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
     }
+
+    func updateSearchResults(for: UISearchController) {
+        searchText = searchController.searchBar.text
+        tableView.reloadData()
+    }
+
+    func getMoviesToDisplay() -> [Movie] {
+        let searchTextNSString = searchText! as NSString
+        if searchTextNSString.length > 0{
+            return movies.filter({
+                (movie: Movie) in
+                var isMatching = false
+
+                if let title = movie.title {
+                    isMatching = title.lowercased().range(of:searchText!.lowercased()) != nil
+                }
+                
+                if (!isMatching) {
+                    if let overview = movie.overview {
+                        isMatching = overview.lowercased().range(of: searchText!.lowercased()) != nil
+                    }
+                }
+                
+                return isMatching
+            })
+        }
+
+        return movies
+    }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let cell = sender as! UITableViewCell
         let indexPath = tableView.indexPath(for: cell)
-        let movie = movies[indexPath!.row]
-        
+        let movie = getMoviesToDisplay()[(indexPath?.row)!]
+        searchController.dismiss(animated: true, completion: nil)
+
         let moviesDetailsViewController = segue.destination as! MovieDetailsViewController
         moviesDetailsViewController.movie = movie
     }
